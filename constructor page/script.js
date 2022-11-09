018d7f8ae272015d96bbdd1000dc6cfa0c02ca7c
boriskobezhikov <bigdiccgorilla2007@gmail.com>
2022-11-09 19:12:07 +0300

    Added search album images

--- constructor page/script.js
+++ constructor page/script.js
@@ -1,12 +1,23 @@
-async function albumImageMaker() {
-let url = "https://ws.audioscrobbler.com/2.0/?method=album.search&album=to%20pimp%20a%20butterfly&api_key=4e448db917e784720c46e1ff1d1f9c14&format=json";
-    let response = await fetch(url);
-    let commits = await response.json();
-    let ggg = commits.results.albummatches.album[0].image[2]['#text']; 
-    let img = document.createElement("img");
-    img.src = ggg;
-    let src = document.getElementById("header");
-    src.appendChild(img);
-}
+async function myFunction() {
+    var imagesDiv = document.getElementById("header");
+    while(imagesDiv.firstChild) {
+        imagesDiv.removeChild(imagesDiv.firstChild);
+    }
+    var input, filter;
+    input = document.getElementById('myInput');
+    filter = input.value;
 
-albumImageMaker();
+    let lastUrl = 'https://ws.audioscrobbler.com/2.0/?method=album.search&';
+    lastUrl = lastUrl + 'album=' + filter + '&limit=10' + '&format=json&api_key=4e448db917e784720c46e1ff1d1f9c14';
+
+    let apiUrl = await fetch(lastUrl);
+    apiUrlJson = await apiUrl.json();
+    let albumsInformation = apiUrlJson.results.albummatches.album;
+    for (let i = 0; i < Object.keys(albumsInformation).length; i++) {
+    let albumImage = albumsInformation[i].image[2]['#text'];
+    let img = document.createElement('img');
+    img.src=albumImage;
+    var src = document.getElementById("header");
+    src.appendChild(img);
+    }
+}
