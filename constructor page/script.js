64799073b97fc6d06258e3dbcb79d4f82ca5ba3c
boriskobezhikov <bigdiccgorilla2007@gmail.com>
2022-11-11 21:46:03 +0300

    Draggable images

--- constructor page/script.js
+++ constructor page/script.js
@@ -8,7 +8,7 @@ async function myFunction() {
     filter = input.value;
 
     let lastUrl = 'https://ws.audioscrobbler.com/2.0/?method=album.search&';
-    lastUrl = lastUrl + 'album=' + filter + '&limit=10' + '&format=json&api_key=4e448db917e784720c46e1ff1d1f9c14';
+    lastUrl = lastUrl + 'album=' + filter  + '&format=json&api_key=4e448db917e784720c46e1ff1d1f9c14';
 
     let apiUrl = await fetch(lastUrl);
     apiUrlJson = await apiUrl.json();
@@ -17,7 +17,27 @@ async function myFunction() {
     let albumImage = albumsInformation[i].image[2]['#text'];
     let img = document.createElement('img');
     img.src=albumImage;
+    img.classList.add("mt-2");
+    img.classList.add("me-2");
+    img.setAttribute("draggable", "true");
+    img.setAttribute("ondragstart", "drag(event)");
+    img.setAttribute("id","album" + i);
     var src = document.getElementById("header");
     src.appendChild(img);
     }
 }
+function allowDrop(ev) {
+    ev.preventDefault();
+  }
+  
+  function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    if ( ev.target.nodeName !== "IMG" ) {
+        ev.target.appendChild(document.getElementById(data));
+    }
+  }
